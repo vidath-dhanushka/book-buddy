5e611a9236a08b38c3f11a1c0c9c169bf59fcbb7
VD Premathilaka <2021cs146@stu.ucsc.cmb.ac.lk>
2024-02-14 20:09:47 +0530

    added backend validations for courier

--- app/models/Courier.model.php
+++ app/models/Courier.model.php
@@ -4,4 +4,52 @@ class Courier extends Model
 {
     protected $table = "Courier";
     public $errors = [];
+
+    protected $allowedColumns = [
+        'company_name',
+        'reg_no',
+        'email',
+        'phone',
+        'rate_first_kg',
+        'rate_extra_kg'
+    ];
+
+    public function validate($data)
+    {
+
+        if (empty($data['company_name'])) {
+            $this->errors['company_name'] = "Please enter company name";
+        }
+
+        if (empty($data['reg_no'])) {
+            $this->errors['reg_no'] = "Please enter registration number of the company";
+        }
+
+        if (empty($data['email'])) {
+            $this->errors['email'] = "Please enter company email";
+        } else
+        if (!filter_var($data['email'],  FILTER_VALIDATE_EMAIL)) {
+            $this->errors['email'] = "Please enter a valid email";
+        }
+
+        if (empty($data['phone'])) {
+            $this->errors['phone'] = "Please enter the phone number";
+        } else
+        if (!preg_match('/^\+[0-9]{11}$/', $data['phone'])) {
+            $this->errors['phone'] = "please enter the number in international standards";
+        }
+
+        if (empty($data['rate_first_kg'])) {
+            $this->errors['rate_first_kg'] = "Please enter the rates for the first kilogram";
+        } else
+        if (!preg_match('/^\d{1,9}(?:,\d{3})*(?:\.\d{2})?$/', $data['rate_first_kg'])) {
+            $this->errors['rate_first_kg'] = "rate should only include numbers with 2 decimals";
+        }
+        if (empty($data['rate_extra_kg'])) {
+            $this->errors['rate_extra_kg'] = "Please enter the rates for the extra kilogram";
+        } else
+        if (!preg_match('/^\d{1,9}(?:,\d{3})*(?:\.\d{2})?$/', $data['rate_extra_kg'])) {
+            $this->errors['rate_extra_kg'] = "rate should only include numbers with 2 decimals";
+        }
+    }
 }
